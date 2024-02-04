1c13b2db208d74479f20243e17e9cfac5b2ae418
ItumelengMphuti <125954886+ItumelengMphuti@users.noreply.github.com>
2024-02-04 22:56:27 +0200

    eddited the landing page

--- Script/index.js
+++ Script/index.js
@@ -1,3 +1,15 @@
 function goToPage() {
     window.location.href = "login.html";
 }
+
+document.addEventListener('scroll', function () {
+    var header = document.querySelector('header');
+    var heroSection = document.querySelector('.hero-section');
+    var scrollTop = window.scrollY;
+
+    if (scrollTop > heroSection.offsetHeight - header.offsetHeight) {
+        header.classList.add('nav-scrolled');
+    } else {
+        header.classList.remove('nav-scrolled', 'line-2');
+    }
+});
